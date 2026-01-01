8f1a009005297cb9dd19180c623d86118aaa8b36
Devenderrawat205 <devenderrawat205@gmail.com>
2026-01-01 19:34:12 +0530

    Delete PythonProject7 directory

--- PythonProject7/BlindQuants/Researchers/admin.py
+++
@@ -1,8 +0,0 @@
-# In Researchers/admin.py
-from django.contrib import admin
-from .models import Stock  # Import the Stock model
-
-admin.site.register(Stock)  # Register the Stock model
-
-
-# Register your models here.
--- PythonProject7/BlindQuants/Researchers/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-print(">>>> TRYING TO LOAD Researchers/apps.py <<<<") # Add this
-
-class BasicResearchersConfig(AppConfig): # Assuming this is your main AppConfig now
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Researchers'
-
-print(">>>> SUCCESSFULLY FINISHED Researchers/apps.py <<<<") # Add this
